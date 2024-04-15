c628e1eb11b45d309cfaeb36fddefbabb0e3399a
aryan-1709 <151056485+aryan-1709@users.noreply.github.com>
2024-04-15 21:29:17 +0530

    Add files via upload

--- receiver.ino
+++ receiver.ino
@@ -1,7 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-//#include <Servo.h>
-//Servo s1;
+#include <Servo.h>
+#define l1 4
+#define l2 5
+#define r1 0
+#define r2 2
+Servo s1;
+Servo s2;
+Servo s3;
+Servo s4;
+Servo s5;
+// #define p D3;
 
 // Structure example to receive data
 // Must match the sender structure
@@ -19,21 +28,19 @@ struct_message myData;
 // Callback function that will be executed when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
+    Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("int: ");
-  Serial.println(myData.a);
-  Serial.print("int: ");
-  Serial.println(myData.b);
-  Serial.print("int: ");
-  Serial.println(myData.c);
-  Serial.print("int: ");
-  Serial.println(myData.d);
-  Serial.print("int: ");
+  Serial.print(myData.a+" ");
+  Serial.print(myData.b+" ");
+  Serial.print(myData.c+" ");
+  Serial.print(myData.d+" ");
   Serial.println(myData.e);
-  
-  Serial.println();
-//  s1.write(myData.c);
+  s1.write(myData.a);
+  s2.write(myData.b);
+  s3.write(myData.c);
+  s4.write(myData.d);
+  s5.write(myData.e);
+
 }
  
 void setup() {
@@ -48,8 +55,17 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
- // s1.attach(1);
-  
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(14, OUTPUT);
+  s1.attach(14);
+  s2.attach(4);
+  s3.attach(5);
+  s4.attach(0);
+  s5.attach(2);
+
   
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
